e440d8c91e0a221f56b87873e6d5966eb28cc526
Your Name <igr.khitrov@gmail.com>
2020-07-30 12:02:05 +0300

    Added WaitException

---
+++ TextSearchHelper/WaitCacheException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextSearchHelper
+{
+    public class WaitCacheException: System.Exception
+    {
+        public WaitCacheException():base("Find call at the time of cache building. You can use \"waitCaching\" to wait cache silently")
+        {
+
+        }
+
+    }
+}
